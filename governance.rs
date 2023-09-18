b58e0de9c0a6c12fd77232a2434ef138a1104162
singularity45 <singularityiscoming2045@gmail.com>
2023-09-18 14:00:00 +0200

    Integrate with decentralized identity providers

--- governance.rs
+++ governance.rs
@@ -3,3 +3,4 @@
 // Improve decentralized exchange (DEX) liquidity
 // Add support for ERC-1155 tokens
 // Fix issues in multi-token smart contracts
+// Fix security vulnerability in random number generation
