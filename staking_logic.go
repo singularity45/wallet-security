89c5a5d57adb96d8b5dc353b1be40c8c56b72b5e
singularity45 <singularityiscoming2045@gmail.com>
2023-09-19 14:00:02 +0200

    Optimize token minting flow

--- staking_logic.go
+++ staking_logic.go
@@ -1,3 +1,4 @@
 // Implement decentralized arbitration in contracts
 // Integrate with decentralized storage systems
 // Integrate with decentralized prediction markets
+// Fix issue in token vesting logic
