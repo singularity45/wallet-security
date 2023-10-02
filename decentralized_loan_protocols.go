f82883f6f687f51ebfe648b8e656f18ec44ca655
singularity45 <singularityiscoming2045@gmail.com>
2023-10-02 14:00:01 +0200

    Optimize storage for off-chain data

--- decentralized_loan_protocols.go
+++ decentralized_loan_protocols.go
@@ -1,3 +1,4 @@
 // Add support for multi-signature wallet contracts
 // Refactor storage layout for better efficiency
 // Add support for multiple consensus mechanisms
+// Fix transaction replay issue
