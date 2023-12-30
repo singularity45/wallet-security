a2561e311ca399226def1ebd10f3b37c69ca0c59
singularity45 <singularityiscoming2045@gmail.com>
2023-12-30 14:00:01 +0200

    Update smart contract tests

--- vesting.go
+++ vesting.go
@@ -4,3 +4,4 @@
 // Enhance cryptographic primitives in contracts
 // Enhance API rate limiting for smart contracts
 // Improve staking and yield farming logic
+// Integrate zk-SNARKs for enhanced privacy
