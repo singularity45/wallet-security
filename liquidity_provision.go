3871033ae506f505063f67e21fdf344b30815f6f
singularity45 <singularityiscoming2045@gmail.com>
2024-09-03 14:00:00 +0200

    Add support for fractional token ownership

--- liquidity_provision.go
+++ liquidity_provision.go
@@ -1,3 +1,4 @@
 // Integrate privacy-enhancing smart contracts
 // Update dependencies for Vyper contract
 // Add support for decentralized marketplaces
+// Add governance proposal expiration feature
