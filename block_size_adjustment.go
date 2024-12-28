8549f3b87ddba3a7bc9e6915731442a67628e593
singularity45 <singularityiscoming2045@gmail.com>
2024-12-28 14:00:01 +0200

    Add support for delegation in governance

--- block_size_adjustment.go
+++ block_size_adjustment.go
@@ -2,3 +2,4 @@
 // Optimize gas usage in contract execution
 // Fix vulnerability in decentralized finance protocols
 // Fix ERC-721 contract issues with transfers
+// Add new feature: on-chain governance
