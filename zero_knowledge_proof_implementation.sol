e50e4c8e79a93b7b1190d4005a0dfe2a7f238544
singularity45 <singularityiscoming2045@gmail.com>
2025-01-14 14:00:00 +0200

    Update smart contract tests

--- zero_knowledge_proof_implementation.sol
+++ zero_knowledge_proof_implementation.sol
@@ -4,3 +4,4 @@
 // Enhance privacy features using zk-STARKs
 // Add support for decentralized auctions
 // Integrate decentralized oracles for data feeds
+// Add governance proposal expiration feature
