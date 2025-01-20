174ae1d92c012ac79f0d775ca455221ea600556b
singularity45 <singularityiscoming2045@gmail.com>
2025-01-20 14:00:01 +0200

    Upgrade to latest Solidity version

--- staking_pool.rs
+++ staking_pool.rs
@@ -1,3 +1,4 @@
 // Fix errors in contract interaction with wallets
 // Enhance decentralized autonomous organizations
 // Add support for decentralized lending platforms
+// Improve gas estimation for transactions
